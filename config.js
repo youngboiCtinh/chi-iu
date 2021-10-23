cb0b9f448af6d39404309fa7fb1f4872ef92b778
youngboiCtinh <92982111+youngboiCtinh@users.noreply.github.com>
2021-10-23 18:22:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Đã lỡ yêu em nhiều <3.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.youtube.com/watch?v=KhTCatAKVpk' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
