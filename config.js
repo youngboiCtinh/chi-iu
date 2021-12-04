56f2fcabf77ad203f0b66678072c59d8fa34bea1
youngboiCtinh <92982111+youngboiCtinh@users.noreply.github.com>
2021-12-04 23:25:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
 const CONFIG = {
-    introTitle: 'Chị bé ơi !!!',
+    introTitle: 'Chị bé ơi hehe',
     introDesc: `Trái đất vốn lạ thường
     Mà sao chị cứ đi nhầm đường
     Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng chị đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng chị đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, chị em tút thui :3',
+    btnNo: 'Không, làm chị em tút thui :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
-    messDesc: 'Đã lỡ yêu chị nhiều <3.',
+    messDesc: 'Hay là mình hẹn hò nha',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.youtube.com/watch?v=06-XXOTP3Gc'
+    messLink: 'https://youtu.be/06-XXOTP3Gc'
+}
